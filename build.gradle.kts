2cacb3f7b1d338a239a931473cf880c3d66b1900
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-04-05 20:48:20 +0900

    Update Spring Boot and JOOQ version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.2.RELEASE"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id("org.owasp.dependencycheck") version "5.2.4"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
+    id("org.springframework.boot") version "2.2.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.owasp.dependencycheck") version "5.2.4"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "com.example"
@@ -14,55 +14,66 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val developmentOnly by configurations.creating
 configurations {
-	runtimeClasspath {
-		extendsFrom(developmentOnly)
-	}
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// Web
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// Database
-	runtimeOnly("mysql:mysql-connector-java")
-	implementation("org.springframework.boot:spring-boot-starter-jooq")
-	implementation("org.flywaydb:flyway-core")
+    // Database
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.flywaydb:flyway-core")
 
-	// Document
-	implementation("io.springfox:springfox-swagger2:2.9.2")
-	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+    // Document
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
-	// Development
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Development
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	// Test
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 dependencyCheck {
-	failBuildOnCVSS = 7.0F
+    failBuildOnCVSS = 7.0F
+
+    suppressionFile = "etc/java/owasp-suppressions.xml"
+
+    // settings to ignore .NET dependency check
+    //
+    // see
+    // https://jeremylong.github.io/DependencyCheck/dependency-check-gradle/configuration.html
+    // https://qiita.com/wrongwrong/items/ef93471c6c22c2202f19
+    analyzers.nuspecEnabled = false
+    analyzers.nugetconfEnabled = false
+    analyzers.assemblyEnabled = false
 }
 
-apply(from = "jooq.gradle")
+apply(from = "etc/java/jooq-flyway.gradle")
