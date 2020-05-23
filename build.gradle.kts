6f7cf3c151aeaaf5570479172b692ef9144d7441
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-05-23 21:35:42 +0900

    Update Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.5.RELEASE"
+    id("org.springframework.boot") version "2.3.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.owasp.dependencycheck") version "5.2.4"
     kotlin("jvm") version "1.3.61"
