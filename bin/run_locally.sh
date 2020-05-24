7a893514a2de7db09f25c95d3f488bf4f97761d9
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-05-24 11:31:11 +0900

    Update build setting

--- bin/run_locally.sh
+++ bin/run_locally.sh
@@ -34,10 +34,12 @@ main() {
         #-Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,ad    dress=5005
       ;;
 
-    # GRADLE_OPTS と --no-daemon は OWASP Dependency Check での以下の issue の回避のため
+    # GRADLE_OPTS、LC_ALL、--no-daemon は OWASP Dependency Check での以下の issue の回避のため
     # https://github.com/jeremylong/DependencyCheck/issues/1742
     build)
       export GRADLE_OPTS="-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8" 
+      export LC_ALL="C.UTF-8"
+
       ./gradlew \
         --no-daemon \
         --stacktrace \
