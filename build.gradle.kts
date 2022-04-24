d2415f5392b6c856a2f5c3f767dccc7d56aefe52
os1ma <39944763+os1ma@users.noreply.github.com>
2022-04-24 09:12:33 +0900

    Update Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val ossIndexUsername = System.getenv("OSS_INDEX_USERNAME")
 val ossIndexPassword = System.getenv("OSS_INDEX_PASSWORD")
 
 plugins {
-    id("org.springframework.boot") version "2.5.12"
+    id("org.springframework.boot") version "2.5.13"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.owasp.dependencycheck") version "6.1.6"
     kotlin("jvm") version "1.6.10"
